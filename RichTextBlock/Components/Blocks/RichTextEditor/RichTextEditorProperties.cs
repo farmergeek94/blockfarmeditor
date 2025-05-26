0ae8a7d1053f1778f8beaceb51bcfbbc7cb0b091
Joshua Hess <jhess@hbs.net>
2025-05-26 08:57:57 -0500

    Added the RichTextBlock

---
+++ RichTextBlock/Components/Blocks/RichTextEditor/RichTextEditorProperties.cs
@@ -0,0 +1,19 @@
+﻿
+using BlockFarmEditor.RCL.Library.Attributes;
+using BlockFarmEditor.RCL.Library.Editors;
+using BlockFarmEditor.RCL.Models.BuilderModels;
+using BlockFarmEditor.Block.RichText.Components.Blocks.RichTextEditor;
+using Umbraco.Cms.Core;
+
+[assembly: BlockFarmEditorBlock("blockfarmeditor.richtexteditor", "Rich Text Editor", PropertiesType = typeof(RichTextEditorProperties), ViewPath = "~/Components/Blocks/RichTextEditor/RichTextEditor.cshtml", Icon = "icon-text")]
+
+namespace BlockFarmEditor.Block.RichText.Components.Blocks.RichTextEditor
+{
+    public class RichTextEditorProperties : IBuilderProperties
+    {
+        [BlockFarmEditorDataType("Richtext editor", "Content")]
+        public RichTextEditorValue? Html { get; set; }
+    }
+
+
+}
